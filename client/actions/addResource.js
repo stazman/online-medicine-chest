bea1b9a8fcd48867e9b79ae6d5f9ac8388378656
Christopher DiStasio <cadistasio@gmail.com>
2020-12-07 23:15:46 -0600

    Stubs use of axios for addResource action

--- client/actions/addResource.js
+++ client/actions/addResource.js
@@ -1,18 +1,28 @@
-export const addResource = (data) => {
+import { ADD_RESOURCE } from './types/resourceTypes';
+import axios from 'axios';
+
+const apiUrl = 'http://localhost:8000/resource';
+
+export const createPost = ({ title, description, url }) => {
   return (dispatch) => {
-    return fetch('http://localhost:3001/api/v1/resources', { 
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-      },
-      method: 'POST',
-      body: JSON.stringify(data)
-    })
-      .then(resp => resp.json())
-      .then(resource => dispatch({
-        type: 'ADD_RESOURCE',
-        payload: resource
+    return axios.post(`${apiUrl}/add`, {title, description, url})
+      .then(response => {
+        dispatch(createPostSuccess(response.data))
       })
-      );
+      .catch(error => {
+        throw(error);
+      });
   };
+};
+
+export const createPostSuccess = (data) => {
+  return {
+    type: ADD_RESOURCE,
+    payload: {
+      _id: data._id,
+      title: data.title,
+      description: data.description,
+      url: data.url
+    }
+  }
 };
