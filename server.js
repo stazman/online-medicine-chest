659394fd161dc1cf73e77cde229df13a68ba95bc
Christopher DiStasio <cadistasio@gmail.com>
2021-03-04 16:54:55 -0600

    Express API runs successfully; removes unneeded

--- server.js
+++ server.js
@@ -1,31 +1,11 @@
 const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
 const app = express();
-const connectDB = require('./config/db')
-const cors = require('cors');
+const connectDB = require('./config/db');
 const PORT = process.env.PORT || 4000;
 
 connectDB();
 
-app.use(cors());
-app.use(express.json());
-app.use(bodyParser.json({limit: '50mb'}));
-
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, './client'));
-app.use(express.static(path.join(__dirname, './client')));
-
-app.use(bodyParser.urlencoded({limit: '50mb', extended: false}));
-
-
-// mongoose.connect('mongodb://127.0.0.1:27017/daily-fitness-advisor-mern', {useNewURLParser: true });
-
-// const connection = mongoose.connection;
-
-// connection.once('open', function(){
-//   console.log('MongoDB db connection established successfully');
-// })
+app.get('/', (req, res) => res.send('API running'));
 
 //TODO:
 // const advisorRouter = require('./routes/AdvisorRoutes');
@@ -40,12 +20,3 @@ app.use(bodyParser.urlencoded({limit: '50mb', extended: false}));
 app.listen(PORT, function(){
   console.log('Server is running on Port ' + PORT);
 })
-
-
-//FOR MONGODB ATLAS INSTEAD OF LOCAL CONFIGURATION:
-
-// require('dotenv').config();
-
-// mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@resources-wadmo.mongodb.net/test`);
-
-// mongo mongodb://$[hostlist]/$[database]?authSource=$[authSource] --username $[username]
