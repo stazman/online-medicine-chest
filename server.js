8d1a9501b22e3f7a439cfc367ab06f39a977696f
Christopher DiStasio <cadistasio@gmail.com>
2021-03-05 13:26:45 -0600

    Adds body parsing middleware; refactors routes

--- server.js
+++ server.js
@@ -5,17 +5,18 @@ const PORT = process.env.PORT || 4000;
 
 connectDB();
 
+//Middleware allowing use of data in req.body
+
+app.use(express.json({
+  extended: false
+}))
+
 app.get('/', (req, res) => res.send('API running'));
 
-//TODO:
-// const advisorRouter = require('./routes/AdvisorRoutes');
-// const journalRouter = require('./routes/JournalRoutes');
-// const resourceRouter = require('./routes/ResourceRoutes');
-
-//TODO:
-// app.use('/advisors', advisorRouter);
-// app.use('/journals', journalRouter);
-// app.use('/resources', resourceRouter);
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/advisors', require('./routes/api/advisors'));
+app.use('/api/journal_entries', require('./routes/api/journal_entries'));
+app.use('/api/resources', require('./routes/api/resources'));
 
 app.listen(PORT, function(){
   console.log('Server is running on Port ' + PORT);
