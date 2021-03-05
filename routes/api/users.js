2de51e14ecdf2433e1f8c79e755c53de1f884129
Christopher DiStasio <cadistasio@gmail.com>
2021-03-05 13:28:57 -0600

    Posts, validates new user information

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult} = require('express-validator');
 
-router.get('/', (req, res) => res.send("User route"));
+router.post('/', [
+  check('name', 'Name is required').not().isEmpty(),
+  check('email', 'Please use a valid email address').isEmail(),
+  check('password', 'Please use a password with six or more characters').isLength({
+    min: 6
+  })
+], (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()){
+    return res.status(422).json({errors: errors.array() })
+  }
+  console.log(req.body);
+  res.send("User route");
+})
 
 module.exports = router;
