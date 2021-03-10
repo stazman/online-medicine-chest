11f3c2676473bc6d01b6ae28d19cdab974a07251
Christopher DiStasio <cadistasio@gmail.com>
2021-03-10 00:37:39 -0600

    Adds middleware, token verification

---
+++ routes/api/auth.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../../middleware/auth');
+
+router.get('/', auth, (req, res) => res.send('Auth route'));
+
+module.exports = router;
