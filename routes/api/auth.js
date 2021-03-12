d804fa59113603abd3c977da0b8d7a7479db0ca0
Christopher DiStasio <cadistasio@gmail.com>
2021-03-12 17:15:18 -0600

    Retrieves user info on auth success with user id

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,6 +2,17 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 
-router.get('/', auth, (req, res) => res.send('Auth route'));
+const User = require('../../models/User');
+
+router.get('/', auth, async (req, res) => {
+
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send('Server error');
+  }
+});
 
 module.exports = router;
