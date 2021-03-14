17a1480afae54312652709eedac22f4b384dc0be
Christopher DiStasio <cadistasio@gmail.com>
2021-03-13 19:06:48 -0600

    Creates, deletes profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -21,4 +21,61 @@ router.get('/me', auth, async (req, res) => {
   }
 })
 
+router.post('/', auth, async (req, res) => {
+
+  const { bio, interests } = req.body;
+
+  const profileFields = {}
+
+  profileFields.user = req.user.id;
+  if (bio) profileFields.bio = bio;
+  if (interests) {
+    profileFields.interests = interests.split(',').map( interest => interest.trim())
+  }
+
+  try {
+
+    let profile = await Profile.findOne({user: req.user.id});
+
+    if (profile) {
+
+      profile = await Profile.findOneAndUpdate(
+        { user: req.user.id },
+        { $set: profileFields },
+        { new: true }
+      );
+      return res.json(profile);
+    }
+
+    profile = new Profile(profileFields);
+
+    await profile.save();
+
+    return res.json(profile);
+
+  } catch(err) {
+    console.log(err.message);
+    res.status(500).send('Server Error');
+  }
+})
+
+router.delete('/', auth, async (req, res) => {
+
+  try {
+
+    await Profile.findOneAndRemove({ user: req.user.id });
+
+    await User.findOneAndRemove({ _id: req.user.id });
+
+    res.json({ msg: 'User and profile deleted' });
+
+  } catch (err){
+
+      console.log(err.message);
+
+      res.status(500).send('Server error');
+
+  }
+})
+
 module.exports = router;
