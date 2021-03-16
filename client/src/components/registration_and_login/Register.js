39fd184e543b30e487bf7b57195f61741abacfaa
Christopher DiStasio <cadistasio@gmail.com>
2021-03-16 15:25:55 -0500

    Removes code unneeded for redux; adds login link

--- client/src/components/registration_and_login/Register.js
+++ client/src/components/registration_and_login/Register.js
@@ -1,5 +1,5 @@
-import React, { Fragment, useState } from 'react'
-import axios from 'axios'
+import React, { Fragment, useState } from 'react';
+import { Link } from "react-router-dom";
 
 const Register = () => {
 
@@ -19,33 +19,9 @@ const Register = () => {
     if (password !== password2){
       console.log('Passwords don\'t match');
     } else {
-      console.log(formData);
-      const newUser = {
-        name,
-        email,
-        password
-      }
-
-      try {
-        const config = {
-          headers: {
-            'Content-Type': "application/json"
-          }
-        }
-
-        const body = JSON.stringify(newUser);
-
-        const res = await axios.post('/api/users', body, config);
-
-        console.log(res.data);
-
-      } catch(err){
-
-          console.error(err.response.data);
-      }
+      console.log("SUCCESS");
     }
   }
-
 
   return (
     <Fragment>
@@ -97,7 +73,7 @@ const Register = () => {
         <input type="submit" className="btn btn-primary" value="Register" />
       </form>
       <p className="my-1">
-        Already have an account? <a href="login.html">Sign In</a>
+        Already have an account? <Link to='/login'>Log In</Link>
       </p>
     </Fragment>
   )
