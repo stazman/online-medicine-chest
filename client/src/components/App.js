b2a4304ec293b54fdf1fb46ca91db4605e010d75
Christopher DiStasio <cadistasio@gmail.com>
2021-03-17 11:34:17 -0500

    Refactors App, Index for best practices

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,81 +1,83 @@
-import React, {Component} from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import TopNavbar from './TopNavbar';
 import Register from './registration_and_login/Register'
 import Login from './registration_and_login/Login'
 import Advisor from './advisor/Advisor';
-import FoodAdvisorsContainer from '../containers/FoodAdvisorsContainer';
-import TrackerHome from './trackers/TrackerHome';
-import JournalHome from './journal/JournalHome';
-import JournalContainer from '../containers/JournalContainer';
-import ResourceHome from './resources/ResourceHome';
-import ResourcesContainer from '../containers/ResourcesContainer';
-import CommunityHome from './community/CommunityHome';
+// import FoodAdvisorsContainer from '../containers/FoodAdvisorsContainer';
+// import TrackerHome from './trackers/TrackerHome';
+// import JournalHome from './journal/JournalHome';
+// import JournalContainer from '../containers/JournalContainer';
+// import ResourceHome from './resources/ResourceHome';
+// import ResourcesContainer from '../containers/ResourcesContainer';
+// import CommunityHome from './community/CommunityHome';
+import { Provider } from 'react-redux';
+import store from '../store'
 import BottomNavbar from './BottomNavbar';
 import { Layout } from './Layout';
 
-class App extends Component {
-  render(){
-    return (
-      <>
+const App = () => {
+  return (
+    <Provider store={store}>
+      <Router>
         <TopNavbar />
-        <Layout>
-          <Switch>
-            {
-              <Route
-                exact path='/'
-                component={Advisor}
-              />
-            }
-            {
-              <Route
-                exact path='/register'
-                component={Register}
-              />
-            }
-            {
-              <Route
-                exact path='/login'
-                component={Login}
-              />
-            }
-            {
-              <Route
-                exact path='/tracker_home'
-                component={TrackerHome}
-              />
-            }
-            {
-              <Route
-                exact path='/journal_home'
-                component={JournalHome}
-              />
-            }
-            {
-              <Route
-                exact path='http://localhost:8000/resource_home'
-                component={ResourceHome}
-              />
-            }
-            {
-              <Route
-                exact path='/community_home'
-                component={CommunityHome}
-              />
-            }
-          </Switch>
+          <Layout>
+            <Switch>
+              {
+                <Route
+                  exact path='/'
+                  component={Advisor}
+                />
+              }
+              {
+                <Route
+                  exact path='/register'
+                  component={Register}
+                />
+              }
+              {
+                <Route
+                  exact path='/login'
+                  component={Login}
+                />
+              }
+              {/* {
+                <Route
+                  exact path='/tracker_home'
+                  component={TrackerHome}
+                />
+              } */}
+              {/* {
+                <Route
+                  exact path='/journal_home'
+                  component={JournalHome}
+                />
+              } */}
+              {/* {
+                <Route
+                  exact path='http://localhost:8000/resource_home'
+                  component={ResourceHome}
+                />
+              } */}
+              {/* {
+                <Route
+                  exact path='/community_home'
+                  component={CommunityHome}
+                />
+              } */}
+            </Switch>
 
-          <FoodAdvisorsContainer />
+            {/* <FoodAdvisorsContainer />
 
-          <JournalContainer />
+            <JournalContainer />
 
-          <ResourcesContainer />
+            <ResourcesContainer /> */}
 
-        </Layout>
+          </Layout>
         <BottomNavbar />
-      </>
-    );
-  }
+      </Router>
+    </Provider>
+  );
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,28 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import manageFoodAdvice from './reducers/manageFoodAdvice';
-import manageJournal from './reducers/manageJournal';
-import manageResources from './reducers/manageResources';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
-import { BrowserRouter as Router } from 'react-router-dom';
-import thunk from 'redux-thunk';
-import { combineReducers } from 'redux';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const rootReducer = combineReducers({
-  manageFoodAdvice,
-  manageJournal,
-  manageResources
-});
-
-let store = createStore(
-  rootReducer,
-  composeEnhancer(applyMiddleware(thunk))
-);
 
 ReactDOM.render(
   <>
@@ -45,11 +23,7 @@ ReactDOM.render(
         }
       `}
     </style>
-    <Provider store={store}>
-      <Router>
         <App />
-      </Router>
-    </Provider>
   </>,
   document.getElementById('root')
 );
