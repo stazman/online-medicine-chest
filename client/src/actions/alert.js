b0abdb63b80a5b8aa87c2430b6ea4bcbd09a409f
Christopher DiStasio <cadistasio@gmail.com>
2021-03-18 09:11:51 -0500

    Removes danger alert on timeout

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -1,7 +1,7 @@
 import uuid from 'uuid';
-import { SET_ALERT, REMOVE_SET } from './types';
+import { SET_ALERT, REMOVE_ALERT } from './types';
 
-export const setAlert = (msg, alertType) => dispatch => {
+export const setAlert = (msg, alertType, timeout = 3000) => dispatch => {
 
   const id = uuid.v4();
 
@@ -9,4 +9,8 @@ export const setAlert = (msg, alertType) => dispatch => {
     type: SET_ALERT,
     payload: { msg, alertType, id }
   })
+
+  setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id
+    }), timeout
+  )
 }
