58ad740c5b8b9f7b39b161edc2d51bcdde5e37da
Christopher DiStasio <cadistasio@gmail.com>
2021-03-18 17:11:33 -0500

    Registers new user, navigates to dashboard

--- client/src/components/registration_and_login/Register.js
+++ client/src/components/registration_and_login/Register.js
@@ -1,11 +1,12 @@
 import React, { Fragment, useState } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import { setAlert } from '../../actions/alert';
-import PropTypes from 'prop-types'
+import { register } from '../../actions/auth';
+import PropTypes from 'prop-types';
 
 
-const Register = ({ setAlert }) => {
+const Register = ({ setAlert, register, isAuthenticated }) => {
 
   const [ formData, setFormData ] = useState({
     name: '',
@@ -23,8 +24,12 @@ const Register = ({ setAlert }) => {
     if (password !== password2){
       setAlert('The passwords don\'t match', 'danger')
     } else {
-      console.log("SUCCESS");
+      register({ name, email, password });
     }
+  }
+
+  if(isAuthenticated){
+    return <Redirect to='/dashboard' />
   }
 
   return (
@@ -85,6 +90,12 @@ const Register = ({ setAlert }) => {
 
 Register.propTypes = {
   setAlert: PropTypes.func.isRequired,
-}
+  register: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
+};
 
-export default connect(null, { setAlert })(Register)
+const mapStatetoProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStatetoProps, { setAlert, register })(Register)
