fda5d5aa2daa65527963550f478daae2d5d4070f
Christopher DiStasio <cadistasio@gmail.com>
2021-03-18 17:13:13 -0500

    Logs in user, navigates to dashboard

--- client/src/components/registration_and_login/Login.js
+++ client/src/components/registration_and_login/Login.js
@@ -1,7 +1,11 @@
 import React, { Fragment, useState } from 'react';
-import { Link } from "react-router-dom"
+import { Link, Redirect } from "react-router-dom";
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import {login} from '../../actions/auth'
 
-const Login = () => {
+
+const Login = ({login, isAuthenticated}) => {
 
   const [ formData, setFormData ] = useState({
     email: '',
@@ -14,9 +18,12 @@ const Login = () => {
 
   const onSubmit = async e => {
     e.preventDefault();
-      console.log("SUCCESS");
+    login(email, password);
   }
 
+  if(isAuthenticated){
+    return <Redirect to='/dashboard' />
+  }
 
   return (
     <Fragment>
@@ -51,4 +58,16 @@ const Login = () => {
   )
 }
 
-export default Login
+Login.propTypes = {
+  // setAlert: PropTypes.func.isRequired,
+  login: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
+};
+
+const mapStatetoProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStatetoProps, {
+  // setAlert,
+  login })(Login)
