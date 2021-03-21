0a59b5b32d53ac0fd42818bf9d1c2b47b7ab607b
Christopher DiStasio <cadistasio@gmail.com>
2021-03-20 21:52:43 -0500

    Creates, stubs profile actions

---
+++ client/src/actions/profile.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import {
+  GET_PROFILE,
+  PROFILE_ERROR,
+  // CREATE_PROFILE,
+  // REMOVE_PROFILE
+} from '../actions/types';
+
+
+export const getCurrentProfile = () => async dispatch => {
+
+  try {
+    const res = axios.get('/api/profile/me')
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data
+    })
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    })
+  }
+}
