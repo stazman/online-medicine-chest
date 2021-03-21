5a4a13f14cffd951fe0200352e490061255720f6
Christopher DiStasio <cadistasio@gmail.com>
2021-03-20 21:45:28 -0500

    Creates, stubs Dashboard component

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,16 +1,40 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { getCurrentProfile } from "../actions/profile";
+import Spinner from "./layouts/Spinner";
 
-const Dashboard = props => {
-  return (
-    <div>
-      Dashboard
-    </div>
-  )
-}
+const Dashboard = ({
+  getCurrentProfile,
+  auth: { user },
+  profile: { profile, loading }
+}) => {
+  useEffect(() => {
+    getCurrentProfile();
+  }, []);
+
+  return loading && profile === null ? (
+    <Spinner />
+  ) : (
+    <>
+      <h1>Dashboard</h1>
+      <h3>Welcome, {user && user.name}</h3>
+
+    { profile !== null ? <>has</> : <>has not</> }
+    </>
+  );
+};
 
 Dashboard.propTypes = {
+  getCurrentProfile: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired,
+};
 
-}
+const mapStatetoProps = state => ({
+  auth: state.auth,
+  profile: state.profile,
+  errors: state.errors
+});
 
-export default Dashboard
+export default connect(mapStatetoProps, { getCurrentProfile })(Dashboard);
