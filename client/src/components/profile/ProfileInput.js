bddb6acd6255e8188ca24122b0d06a4a573e0a06
Christopher DiStasio <cadistasio@gmail.com>
2021-03-22 16:53:16 -0500

    Creates, stubs profile input

---
+++ client/src/components/profile/ProfileInput.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+const ProfileInput = props => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+ProfileInput.propTypes = {
+
+}
+
+export default ProfileInput;
+
+//create form
+//map state to props for profile
+//connect and dispatch
+//no actions here
+//use auth to ensure profile created is auth
+
