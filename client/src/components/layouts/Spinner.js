0378e0890b76222a2f4302fdc4d03ca87aed62b1
Christopher DiStasio <cadistasio@gmail.com>
2021-03-21 21:03:20 -0500

    Adds loading gif

---
+++ client/src/components/layouts/Spinner.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import spinner from './loading.gif';
+
+export default () => (
+  <>
+    <img
+      src={spinner}
+      style={{ width: '200px', margin: 'auto', display: 'block' }}
+      alt='Loading ...'
+    />
+  </>
+)
