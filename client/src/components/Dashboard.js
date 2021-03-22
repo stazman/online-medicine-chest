a68ca66cac3c86eea50354053f4da3fa531b484e
Christopher DiStasio <cadistasio@gmail.com>
2021-03-22 16:18:07 -0500

    Returns create profile message if none existing

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,8 +1,11 @@
 import React, { useEffect } from "react";
+import { Button } from 'react-bootstrap';
+import ButtonStyles from './styles/ButtonStyles';
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { getCurrentProfile } from "../actions/profile";
 import Spinner from "./layouts/Spinner";
+
 
 const Dashboard = ({
   getCurrentProfile,
@@ -11,7 +14,8 @@ const Dashboard = ({
 }) => {
   useEffect(() => {
     getCurrentProfile();
-  }, []);
+  }, [getCurrentProfile])
+
 
   return loading && profile === null ? (
     <Spinner />
@@ -20,7 +24,15 @@ const Dashboard = ({
       <h1>Dashboard</h1>
       <h3>Welcome, {user && user.name}</h3>
 
-    { profile !== null ? <>has</> : <>has not</> }
+      { profile !== null ? <>Profile</> :
+      <ButtonStyles>
+        <p>You have not set up a profile. Please add some info ...</p>
+          <br></br>
+          <Button variant="submit-sm" href='create-profile'>
+            Create Profile
+          </Button>
+      </ButtonStyles>
+      }
     </>
   );
 };
