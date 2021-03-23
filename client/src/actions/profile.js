eda4749918be4e2940d3488c6350e87556f976d3
Christopher DiStasio <cadistasio@gmail.com>
2021-03-23 15:04:39 -0500

    Adds, completes createProfile

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,16 +1,15 @@
 import axios from 'axios';
 import {
   GET_PROFILE,
-  PROFILE_ERROR,
-  // CREATE_PROFILE,
-  // REMOVE_PROFILE
+  PROFILE_ERROR
 } from '../actions/types';
+import { setAlert } from './alert';
 
 
 export const getCurrentProfile = () => async dispatch => {
 
   try {
-    const res = await axios.get('/api/profile/me')
+    const res = await axios.get('/api/profile/me');
     dispatch({
       type: GET_PROFILE,
       payload: res.data
@@ -22,3 +21,38 @@ export const getCurrentProfile = () => async dispatch => {
     })
   }
 }
+
+export const createProfile = (formData, history, edit = false) => async dispatch => {
+
+  try {
+    const config = {
+      headers: {'Content-Type' : 'application/json'}
+    };
+
+    const res = await axios.post('/api/profile', formData, config);
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data
+    });
+
+    dispatch( setAlert(edit ? 'Your profile has been updated' : 'Your profile has been created', 'success') );
+
+    if (!edit){
+      history.push('/dashboard');
+    }
+
+  } catch(err){
+
+    const errors = err.response.data.errors;
+
+    if(errors){
+      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')))
+    }
+
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
