69a805d95c605cd0c43f35a9407abf94e5088d75
Christopher DiStasio <cadistasio@gmail.com>
2021-03-23 17:06:49 -0500

    Moves dashboard component to own dir

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,10 +1,11 @@
 import React, { useEffect } from "react";
 import { Button } from 'react-bootstrap';
-import ButtonStyles from './styles/ButtonStyles';
+import ButtonStyles from '../styles/ButtonStyles';
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import { getCurrentProfile } from "../actions/profile";
-import Spinner from "./layouts/Spinner";
+import { getCurrentProfile } from "../../actions/profile";
+import Spinner from "../layouts/Spinner";
+import DashboardActions from './DashboardActions';
 
 
 const Dashboard = ({
@@ -24,7 +25,11 @@ const Dashboard = ({
       <h1>Dashboard</h1>
       <h3>Welcome, {user && user.name}</h3>
 
-      { profile !== null ? <>Profile</> :
+
+      { profile !== null ?
+        <>
+          <DashboardActions />
+        </> :
       <ButtonStyles>
         <p>You have not set up a profile. Please add some info ...</p>
           <br></br>
