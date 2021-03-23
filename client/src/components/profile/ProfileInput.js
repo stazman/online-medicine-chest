b88f588814f6ed708315d8ef30630ef97e75990a
Christopher DiStasio <cadistasio@gmail.com>
2021-03-22 20:56:24 -0500

    Adds, stubs profile input component

--- client/src/components/profile/ProfileInput.js
+++ client/src/components/profile/ProfileInput.js
@@ -1,13 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+
 
 const ProfileInput = props => {
-  return (
-    <div>
 
-    </div>
-  )
+  const [formData, setFormData] = useState({
+    bio: '',
+    interests: []
+  });
+
+  const {
+    bio,
+    interests
+  } = setFormData;
+
+  return (
+    <>
+      <h1 className="large text-primary">
+        Create Your Profile
+      </h1>
+      <form className="form">
+        <div className="form-group">
+            <h3>Let us know a little about you!</h3>
+            <br></br>
+            <textarea
+              id='bio'
+              name='bio'
+              placeholder="A short bio of yourself"
+              rows='3'
+              cols='50'>
+            </textarea>
+        </div>
+        <div className="form-group">
+          <input type="text" placeholder="What are some of your interests?" name="interests" size='50' />
+          <small className="form-text">Please use comma separated values (eg.
+            reading, hiking, cats)
+          </small>
+        </div>
+
+        <input type="submit" className="btn btn-primary my-1" />
+        <br></br><br></br>
+        <a className="btn btn-light my-1" href="dashboard.html">Go Back</a>
+      </form>
+      </>
+)
 }
 
 ProfileInput.propTypes = {
@@ -15,10 +51,3 @@ ProfileInput.propTypes = {
 }
 
 export default ProfileInput;
-
-//create form
-//map state to props for profile
-//connect and dispatch
-//no actions here
-//use auth to ensure profile created is auth
-
