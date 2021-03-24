391be07f6b0341104b612dbe672ef711adcc019a
Christopher DiStasio <cadistasio@gmail.com>
2021-03-24 16:37:38 -0500

    Adds styles for show buttons, list

--- client/src/components/styles/ButtonStyles.js
+++ client/src/components/styles/ButtonStyles.js
@@ -2,11 +2,6 @@ import styled from 'styled-components';
 
 const ButtonStyles = styled.div`
 
-  .btn-main {
-    background-color: navy;
-    color: white;
-    font-size: 1.8rem;
-  }
 
   .btn-submit {
     font-size: 2rem;
@@ -14,10 +9,33 @@ const ButtonStyles = styled.div`
     color: white;
   }
 
+  .btn-main {
+    background-color: navy;
+    color: white;
+    font-size: 1.8rem;
+  }
+
   .btn-submit-sm {
     font-size: 1.6rem;
     background-color: navy;
     color: white;
+  }
+
+  .btn-show-vsmall {
+    font-size: .8rem;
+    background-color: navy;
+    color: white;
+  }
+
+  li.list-button{
+    display: inline;
+    list-style: none;
+  }
+
+  ul.inline {
+    display: inline-block;
+    margin: 0;
+    padding: 0;
   }
 `;
 
