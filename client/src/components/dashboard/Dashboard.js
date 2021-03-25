9a6b4c9a222333b108e26ce2a0df6593677fd25e
Christopher DiStasio <cadistasio@gmail.com>
2021-03-25 16:41:36 -0500

    Styles Dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,12 +1,68 @@
 import React, { useEffect } from "react";
-import { Button } from 'react-bootstrap';
-import ButtonStyles from '../styles/ButtonStyles';
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { getCurrentProfile } from "../../actions/profile";
 import Spinner from "../layouts/Spinner";
 import DashboardActions from './DashboardActions';
+import styled from 'styled-components';
+import { device } from '../styles/device';
+import { Button } from 'react-bootstrap';
+import ButtonStyles from '../styles/ButtonStyles';
+import TextStyles from '../styles/TextStyles';
 
+
+const MediaQueries = styled.div`
+  @media ${device.mobileS} {
+    h3 {
+      font-size: 2rem;
+    }
+    p, a {
+      font-size: 1.4rem;
+    }
+    button.btn-submit {
+      font-size: 1.4rem;
+    }
+    .link-standard {
+      font-size: 1.4rem
+    }
+    .btn-show-vsmall {
+      font-size: 1rem;
+    }
+  }
+
+  @media ${device.tabletS} {
+    h3 {
+      font-size: 2.6rem;
+    }
+    a, p {
+      font-size: 1.8rem;
+    }
+    button.btn-submit {
+      font-size: 1.8rem;
+    }
+    .link-standard {
+      font-size: 1.8rem
+    }
+    .btn-show-vsmall {
+      font-size: 1.2rem;
+    }
+  }
+
+  @media ${device.laptop} {
+    h3 {
+      font-size: 3rem;
+    }
+    a, p, li {
+      font-size: 2rem;
+    }
+    button.btn-submit {
+      font-size: 2rem;
+    }
+    .btn-show-vsmall {
+      font-size: 1.6rem;
+    }
+  }
+`
 
 const Dashboard = ({
   getCurrentProfile,
@@ -19,45 +75,49 @@ const Dashboard = ({
 
 
   return loading && profile === null ? (
+
     <Spinner />
   ) : (
-    <ButtonStyles>
-      <h3>Welcome, {user && user.name}!</h3>
-        <br></br><br></br>
-        { profile !== null ?
-          <>
-            <div>
-              <h3>
-                Your current bio:
-              </h3>
-              <p>{profile.bio}</p>
-            </div>
-            <br></br><br></br>
-            <div>
-              <h3>
-                Your current interests:
-              </h3>
+    <MediaQueries>
+      <TextStyles>
+        <ButtonStyles>
+          <h3>Welcome, {user && user.name}!</h3>
+          <br></br><br></br>
+          { profile !== null ?
+            <>
+              <div>
+                <h3>
+                  Your current bio:
+                </h3>
+                <p>{profile.bio}</p>
+              </div>
+              <br></br><br></br>
+              <div>
+                <h3>
+                  Your current interests:
+                </h3>
+                <br></br>
+                <ul className='inline'>
+                  {profile.interests.map( i =>
+                    <li className='list-button'><Button variant='show-vsmall'>{i}</Button>&nbsp;&nbsp;</li>
+                  )}
+                </ul>
+              </div>
+              <br></br><br></br><br></br>
+              <DashboardActions />
+            </>
+          :
+            <>
+              <p>You have not set up a profile. Please add some info ...</p>
               <br></br>
-              <ul className='inline'>
-                {profile.interests.map( i =>
-                  <li className='list-button'><Button variant='show-vsmall'>{i}</Button>&nbsp;&nbsp;</li>
-                )}
-              </ul>
-            </div>
-            <br></br><br></br><br></br>
-            <DashboardActions />
-          </>
-        :
-        <>
-            <p>You have not set up a profile. Please add some info ...</p>
-              <br></br>
-              <Button variant="submit-sm" href='create-profile'>
+              <Button variant="submit" href='create-profile'>
                 Create Profile
               </Button>
-        </>
-      }
-    </ButtonStyles>
-
+            </>
+          }
+        </ButtonStyles>
+      </TextStyles>
+    </MediaQueries>
   );
 };
 
