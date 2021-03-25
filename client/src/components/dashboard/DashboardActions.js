59f3e0d4f15d7e331fa62e1cdcfd6f39752aa3cf
Christopher DiStasio <cadistasio@gmail.com>
2021-03-25 13:44:43 -0500

    Makes edit profile button responsive; edits style

--- client/src/components/dashboard/DashboardActions.js
+++ client/src/components/dashboard/DashboardActions.js
@@ -1,20 +1,40 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 import ButtonStyles from '../styles/ButtonStyles';
+import styled from 'styled-components';
+import { device } from '../styles/device';
 
+
+const MediaQueries = styled.div`
+  @media ${device.mobileS} {
+    .btn-submit {
+      font-size: 1.4rem;
+    }
+  }
+
+  @media ${device.tabletS} {
+    .btn-submit {
+      font-size: 1.8rem;
+    }
+  }
+
+  @media ${device.laptop} {
+    .btn-submit {
+      font-size: 2rem;
+    }
+  }
+`
 
 const DashboardActions = () => {
   return (
     <div>
-      <ButtonStyles>
-        <Button variant="submit-sm" href='edit-profile'>
-          Edit Profile
-        </Button>
-        &nbsp;&nbsp;
-        <Button variant="submit-sm" href='add-interest'>
-          Add an Interest
-        </Button>
-      </ButtonStyles>
+      <MediaQueries>
+        <ButtonStyles>
+          <Button variant="submit" href='edit-profile'>
+            Edit Profile
+          </Button>
+        </ButtonStyles>
+      </MediaQueries>
     </div>
   )
 }
