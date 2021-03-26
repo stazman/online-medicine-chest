0ef6a3b90ab863d638a0c42928115f50df01a43a
Christopher DiStasio <cadistasio@gmail.com>
2021-03-26 11:50:14 -0500

    Adds proptypes; refactors Layout

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import PropTypes from 'prop-types';
 
-export const Layout = (props) => {
+
+export const Layout = ({children}) => {
   return (
     <Container style={{marginTop: '5%'}}>
-      {props.children}
+      {children}
     </Container>
   );
+};
+
+Layout.propTypes = {
+  children: PropTypes.func.isRequired
 };
