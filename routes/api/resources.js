1ef34f094716e9d58a15032e8ec3ea45f6954476
Christopher DiStasio <cadistasio@gmail.com>
2021-04-03 16:14:51 -0500

    Retrieves resources for a given user

--- routes/api/resources.js
+++ routes/api/resources.js
@@ -5,6 +5,24 @@ const Resource = require('../../models/Resource');
 const { check, validationResult} = require('express-validator');
 
 
+router.get('/me', auth, async (req, res) => {
+
+  try {
+
+    const resources = await Resource.find({user: req.user.id});
+
+    if(!resources){
+      res.status(400).json({ errors: [{msg: 'There are no resources for this user'}]});
+    }
+
+    res.json(resources)
+
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
 router.post('/', auth, [
   check('url', 'Please write the website\'s url (web address) or copy and paste it here.').exists(),
   check('url', 'Please use a valid email address').isURL()
